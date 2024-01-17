0d1bb31ddc290605e23d26cacd4cbeb1f1d46409
quniob <me@quniob.ru>
2024-01-18 00:43:24 +0500

    Создан шаблон для страницы востребованности - добавлен первый график и таблица

--- info_pages/views.py
+++ info_pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 def main_page(request):
@@ -7,7 +8,13 @@ def main_page(request):
 
 
 def demand_page(request):
-    return HttpResponse('demand')
+    data = models.Demand_average_salary.objects.all()
+    images = models.Demand_charts.objects.get(id=1)
+    context = {
+        "data": data,
+        "images": images,
+    }
+    return render(request, "demand_page.html", context=context)
 
 
 def geography_page(request):
