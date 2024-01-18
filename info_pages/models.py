0503ca59f37e401c5c0d1156e58d20200812c114
quniob <me@quniob.ru>
2024-01-18 20:18:19 +0500

    Добавлены модели для страницы География

--- info_pages/models.py
+++ info_pages/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Модели для страницы "Востребованность"
+
 class Demand_average_salary(models.Model):
     year = models.IntegerField()
     salary = models.IntegerField()
@@ -24,3 +26,31 @@ class Demand_charts(models.Model):
     average_amount = models.ImageField(upload_to='demand')
     devops_salary = models.ImageField(upload_to='demand')
     devops_amount = models.ImageField(upload_to='demand')
+
+
+# Модели для страницы "География"
+class Geography_salary(models.Model):
+    city = models.CharField()
+    salary = models.IntegerField()
+
+
+class Geography_rating(models.Model):
+    city = models.CharField()
+    rate = models.FloatField()
+
+
+class Geography_devops_salary(models.Model):
+    city = models.CharField()
+    salary = models.IntegerField()
+
+
+class Geography_devops_rating(models.Model):
+    city = models.CharField()
+    rate = models.FloatField()
+
+
+class Geography_charts(models.Model):
+    salary = models.ImageField(upload_to='demand')
+    rating = models.ImageField(upload_to='demand')
+    devops_salary = models.ImageField(upload_to='demand')
+    devops_rating = models.ImageField(upload_to='demand')
