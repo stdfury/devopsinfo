9791a38775878f4bfa950c7bf8264de36afc48d5
quniob <me@quniob.ru>
2024-01-20 13:12:12 +0500

    Фиксы для работы на сервере

--- devopsinfo/settings.py
+++ devopsinfo/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/usr/src/app/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
